ad2e51949d027370c1bce03e59a94e0928162e0a
Nathan Vital <nathan.vital@outlook.com>
2021-07-28 20:34:05 -0300

    adding package.json and starting to work with other modules

--- main.js
+++ main.js
@@ -1,33 +1,5 @@
-const {readFile, writeFile} = require('fs')
+const _ = require('lodash')
 
-console.log('start')
-readFile('./content/first.txt','utf8', (err,result) => {
-    if(err){
-        console.log(err)
-        return;
-        //or return null;
-    }
-    const first = result;
-    readFile('./content/second.txt','utf8', (err, result) =>{
-        if(err){
-            console.log(err)
-            return;
-            //or return null;
-        }
-        const second = result;
-        writeFile(
-        './content/result-async.txt',
-        `Here is the result: ${first}, ${second}`, {flag: 'a'}
-        ,(err, result)=>{
-        if(err){
-            console.log(err);
-            return;
-        }
-        console.log(result)
-        console.log('done with this task');
-        //final result which appears on /content/result-async.txt
-        })
-    })
-})
-
-console.log('starting next task')
+const items = [1,[2,[3,[4]]]]
+const newItems = _.flattenDeep(items);
+console.log(newItems)
